2001d318daf412d03d3428d4744bb506e0e6dd97
catp0wer <catp0wer@mail.ru>
2019-09-13 21:32:23 +0300

    adding to call the function from Part2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String [ ] args){
+        System.out.println("PART I");
         Part1 gene = new Part1();
         gene.testSimpleGene();
+
+        System.out.println("");
+        System.out.println("PART II");
+        Part2 gene2 = new Part2();
+        gene2.testSimpleGene();
     }
 }
